14142e486d08ba83bde0cf951ae3f32275755258
Sagar Bhatia <sagarbhatia722@gmail.com>
2021-12-14 22:28:47 +0530

    designing

--- codeial/controllers/home_controller.js
+++ codeial/controllers/home_controller.js
@@ -23,7 +23,7 @@ module.exports.home = function (req, res) {
     .exec(function (err, posts) {
       User.find({}, function (err, users) {
         return res.render("home", {
-          title: "Codeial | Home",
+          title: "Home",
           posts: posts,
           all_users: users,
         });
