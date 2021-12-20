27fd0e348b07928cf4e430740eb566112a79cee2
Sagar Bhatia <sagarbhatia722@gmail.com>
2021-12-20 14:28:19 +0530

    converted to ajax and made some api folder

---
+++ codeial/controllers/api/v1/posts_api.js
@@ -0,0 +1,17 @@
+const Post = require("../../../models/post");
+
+module.exports.index = async function (req, res) {
+  let posts = await Post.find({})
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+
+  return res.json(200, {
+    message: "list of posts",
+    posts: posts,
+  });
+};
--- codeial/controllers/posts_controller.js
+++ codeial/controllers/posts_controller.js
@@ -4,10 +4,19 @@ const { setFlash } = require("../config/middleware");
 
 module.exports.create = async function (req, res) {
   try {
-    await Post.create({
+    let post = await Post.create({
       content: req.body.content,
       user: req.user._id,
     });
+
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "post created!!",
+      });
+    }
 
     req.flash("success", "POST PUBLISHED!");
 
